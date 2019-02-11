263ea00f38a430cf578131343658635181795939
erikf.knudsen <erkn@itu.dk>
2019-02-11 16:25:28 +0100

    Added MyClass

---
+++ MyClass.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Erik Frej Knudsen on 2019-02-11.
+//
+
+#include "MyClass.h"
---
+++ MyClass.h
@@ -0,0 +1,14 @@
+//
+// Created by Erik Frej Knudsen on 2019-02-11.
+//
+
+#ifndef C_TESTGIT_MYCLASS_H
+#define C_TESTGIT_MYCLASS_H
+
+
+class MyClass {
+
+};
+
+
+#endif //C_TESTGIT_MYCLASS_H
